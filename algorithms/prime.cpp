e5c106e7216e3b560dd89727882c9b2a8e96e32d
AaronHe7 <aaronhe07@gmail.com>
2022-12-18 17:10:21 -0500

    Add prime factorization and fix bugs

--- algorithms/prime.cpp
+++ algorithms/prime.cpp
@@ -22,6 +22,9 @@ void generate_primes(int mx) {
 		}
 		if (sieve[i]) {
 			primes.push_back(i);
+			if (1LL * i * i > mx) {
+				continue;
+			}
 			for (int j = i * i; j <= mx; j += i) {
 				sieve[j] = false;
 			}
@@ -43,4 +46,39 @@ bool is_prime(int n) {
 		}
 	}
 	return n >= 2;
+}
+// Theorem: each integer n >= 2 is prime or has a prime factor <= sqrt(n)
+// Theorem 2: the second smallest factor of n >= 2 is prime
+template <typename T>
+vector<pair<T, int>> prime_factors(T n) {
+	vector<pair<T, int>> res;
+	int j = 0;
+	for (int i = 2; 1LL * i * i <= n; i++) {
+		if (j < primes.size() && i <= primes[j]) {
+			i = primes[j];
+			j++;
+		}
+		if (i * i > n) break;
+		if (n % i == 0) {
+			int count = 0;
+			while (n % i == 0) {
+				n /= i;
+				count++;
+			}
+			res.emplace_back(i, count);
+		}
+	}
+	if (n > 1) {
+		res.emplace_back(n, 1);
+	}
+	return res;
+}
+template <typename T>
+vector<T> unique_prime_factors(T n) {
+	vector<pair<T, int>> factors = prime_factors(n);
+	vector<T> res;
+	for (auto &[p, e] : factors) {
+		res.push_back(p);
+	}
+	return res;
 }
